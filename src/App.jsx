99054f335621b896ee4a763ae7ecaa6992e7a538
jittawee <jtwe.esr@gmail.com>
2025-08-12 15:17:38 +0700

    App.jsx: Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,44 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-// Layout หลัก// 
-import { Layout } from './components/Layout';
+// Layout หลัก//
+import { Layout } from "./components/Layout";
 
 // Views/Pages ต่างๆ ที่มีใน Web App//
-import { HomePage } from './views/HomePage';
-import { BrowsePage } from './views/BrowsePage';
-import { AboutPage } from './views/AboutPage';
-import { ContactPage } from './views/ContactPage';
-import { SignInPage } from './views/SignInPage';
-import { RegisterPage } from './views/RegisterPage';
-import { UserProfilePage } from './views/UserProfilePage';
-import { ShopPage } from './views/ShopPage';
-import { CartPage } from './views/CartPage';
-import { CheckoutPage } from './views/CheckoutPage';
-import { OrderConfirmationPage } from './views/OrderConfirmationPage';
+import { HomePage } from "./views/HomePage";
+import { BrowsePage } from "./views/BrowsePage";
+import { AboutPage } from "./views/AboutPage";
+import { ContactPage } from "./views/ContactPage";
+import { SignInPage } from "./views/SignInPage";
+import { RegisterPage } from "./views/RegisterPage";
+import { UserProfilePage } from "./views/UserProfilePage";
+import { ShopPage } from "./views/ShopPage";
+import { CartPage } from "./views/CartPage";
+import { CheckoutPage } from "./views/CheckoutPage";
+import { OrderConfirmationPage } from "./views/OrderConfirmationPage";
 
-export const App = () => {
-  return (
-    <BrowserRouter>
-    <Routes>
-      {/* layout ที่เป็น wrapper ที่คุมทุก Page อยู่ */}
-      <Route path="/" element={<Layout />}>
-      <Route index={true} element={<HomePage />} />
-      <Route path="browse" element={<BrowsePage />} />
-      <Route path="about" element={<AboutPage />} />
-      <Route path="contact" element={<ContactPage />} />
-      <Route path="signin" element={<SignInPage />} />
-      <Route path="register" element={<RegisterPage />} />
-      <Route path="profile" element={<UserProfilePage />} />
-      <Route path="shop" element={<ShopPage />} />
-      <Route path="cart" element={<CartPage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
-      <Route path="confirmation" element={<OrderConfirmationPage />} />
-      </Route>
-      
-    </Routes>
-    </BrowserRouter>
-  )
-}
+// สร้าง router object
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />, // Layout เป็น wrapper หลัก
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "browse", element: <BrowsePage /> },
+      { path: "about", element: <AboutPage /> },
+      { path: "contact", element: <ContactPage /> },
+      { path: "signin", element: <SignInPage /> },
+      { path: "register", element: <RegisterPage /> },
+      { path: "profile", element: <UserProfilePage /> },
+      { path: "shop", element: <ShopPage /> },
+      { path: "cart", element: <CartPage /> },
+      { path: "checkout", element: <CheckoutPage /> },
+      { path: "confirmation", element: <OrderConfirmationPage /> },
+    ],
+  },
+]);
 
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
+export default App;
